46034eb7597e402f36e0a96ce59bea514612261d
zgs225 <zgs225@gmail.com>
2015-11-19 15:06:21 +0800

    修改权限Seeder文件

--- database/seeds/Access/PermissionTableSeeder.php
+++ database/seeds/Access/PermissionTableSeeder.php
@@ -34,7 +34,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$viewBackend = new $permission_model;
 		$viewBackend->name = 'view-backend';
-		$viewBackend->display_name = 'View Backend';
+		$viewBackend->display_name = '查看后台';
 		$viewBackend->system = true;
 		$viewBackend->group_id = 1;
 		$viewBackend->sort = 1;
@@ -45,7 +45,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$viewAccessManagement = new $permission_model;
 		$viewAccessManagement->name = 'view-access-management';
-		$viewAccessManagement->display_name = 'View Access Management';
+		$viewAccessManagement->display_name = '查看访问管理';
 		$viewAccessManagement->system = true;
 		$viewAccessManagement->group_id = 1;
 		$viewAccessManagement->sort = 2;
@@ -63,7 +63,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$createUsers = new $permission_model;
 		$createUsers->name = 'create-users';
-		$createUsers->display_name = 'Create Users';
+		$createUsers->display_name = '创建用户';
 		$createUsers->system = true;
 		$createUsers->group_id = 2;
 		$createUsers->sort = 5;
@@ -74,7 +74,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$editUsers = new $permission_model;
 		$editUsers->name = 'edit-users';
-		$editUsers->display_name = 'Edit Users';
+		$editUsers->display_name = '编辑用户';
 		$editUsers->system = true;
 		$editUsers->group_id = 2;
 		$editUsers->sort = 6;
@@ -85,7 +85,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$deleteUsers = new $permission_model;
 		$deleteUsers->name = 'delete-users';
-		$deleteUsers->display_name = 'Delete Users';
+		$deleteUsers->display_name = '删除用户';
 		$deleteUsers->system = true;
 		$deleteUsers->group_id = 2;
 		$deleteUsers->sort = 7;
@@ -96,7 +96,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$changeUserPassword = new $permission_model;
 		$changeUserPassword->name = 'change-user-password';
-		$changeUserPassword->display_name = 'Change User Password';
+		$changeUserPassword->display_name = '修改用户密码';
 		$changeUserPassword->system = true;
 		$changeUserPassword->group_id = 2;
 		$changeUserPassword->sort = 8;
@@ -107,7 +107,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$deactivateUser = new $permission_model;
 		$deactivateUser->name = 'deactivate-users';
-		$deactivateUser->display_name = 'Deactivate Users';
+		$deactivateUser->display_name = '冻结用户';
 		$deactivateUser->system = true;
 		$deactivateUser->group_id = 2;
 		$deactivateUser->sort = 9;
@@ -118,7 +118,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$banUsers = new $permission_model;
 		$banUsers->name = 'ban-users';
-		$banUsers->display_name = 'Ban Users';
+		$banUsers->display_name = '封禁用户';
 		$banUsers->system = true;
 		$banUsers->group_id = 2;
 		$banUsers->sort = 10;
@@ -129,7 +129,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$reactivateUser = new $permission_model;
 		$reactivateUser->name = 'reactivate-users';
-		$reactivateUser->display_name = 'Re-Activate Users';
+		$reactivateUser->display_name = '重激活用户';
 		$reactivateUser->system = true;
 		$reactivateUser->group_id = 2;
 		$reactivateUser->sort = 11;
@@ -140,7 +140,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$unbanUser = new $permission_model;
 		$unbanUser->name = 'unban-users';
-		$unbanUser->display_name = 'Un-Ban Users';
+		$unbanUser->display_name = '解禁用户';
 		$unbanUser->system = true;
 		$unbanUser->group_id = 2;
 		$unbanUser->sort = 12;
@@ -151,7 +151,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$undeleteUser = new $permission_model;
 		$undeleteUser->name = 'undelete-users';
-		$undeleteUser->display_name = 'Restore Users';
+		$undeleteUser->display_name = '恢复用户';
 		$undeleteUser->system = true;
 		$undeleteUser->group_id = 2;
 		$undeleteUser->sort = 13;
@@ -162,7 +162,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$permanentlyDeleteUser = new $permission_model;
 		$permanentlyDeleteUser->name = 'permanently-delete-users';
-		$permanentlyDeleteUser->display_name = 'Permanently Delete Users';
+		$permanentlyDeleteUser->display_name = '永久删除用户';
 		$permanentlyDeleteUser->system = true;
 		$permanentlyDeleteUser->group_id = 2;
 		$permanentlyDeleteUser->sort = 14;
@@ -173,7 +173,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$resendConfirmationEmail = new $permission_model;
 		$resendConfirmationEmail->name = 'resend-user-confirmation-email';
-		$resendConfirmationEmail->display_name = 'Resend Confirmation E-mail';
+		$resendConfirmationEmail->display_name = '重发送用户确认邮件';
 		$resendConfirmationEmail->system = true;
 		$resendConfirmationEmail->group_id = 2;
 		$resendConfirmationEmail->sort = 15;
@@ -187,7 +187,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$createRoles = new $permission_model;
 		$createRoles->name = 'create-roles';
-		$createRoles->display_name = 'Create Roles';
+		$createRoles->display_name = '创建角色';
 		$createRoles->system = true;
 		$createRoles->group_id = 3;
 		$createRoles->sort = 2;
@@ -198,7 +198,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$editRoles = new $permission_model;
 		$editRoles->name = 'edit-roles';
-		$editRoles->display_name = 'Edit Roles';
+		$editRoles->display_name = '编辑角色';
 		$editRoles->system = true;
 		$editRoles->group_id = 3;
 		$editRoles->sort = 3;
@@ -209,7 +209,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$deleteRoles = new $permission_model;
 		$deleteRoles->name = 'delete-roles';
-		$deleteRoles->display_name = 'Delete Roles';
+		$deleteRoles->display_name = '删除角色';
 		$deleteRoles->system = true;
 		$deleteRoles->group_id = 3;
 		$deleteRoles->sort = 4;
@@ -223,7 +223,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$createPermissionGroups = new $permission_model;
 		$createPermissionGroups->name = 'create-permission-groups';
-		$createPermissionGroups->display_name = 'Create Permission Groups';
+		$createPermissionGroups->display_name = '创建权限组';
 		$createPermissionGroups->system = true;
 		$createPermissionGroups->group_id = 4;
 		$createPermissionGroups->sort = 1;
@@ -234,7 +234,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$editPermissionGroups = new $permission_model;
 		$editPermissionGroups->name = 'edit-permission-groups';
-		$editPermissionGroups->display_name = 'Edit Permission Groups';
+		$editPermissionGroups->display_name = '编辑权限组';
 		$editPermissionGroups->system = true;
 		$editPermissionGroups->group_id = 4;
 		$editPermissionGroups->sort = 2;
@@ -245,7 +245,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$deletePermissionGroups = new $permission_model;
 		$deletePermissionGroups->name = 'delete-permission-groups';
-		$deletePermissionGroups->display_name = 'Delete Permission Groups';
+		$deletePermissionGroups->display_name = '删除权限组';
 		$deletePermissionGroups->system = true;
 		$deletePermissionGroups->group_id = 4;
 		$deletePermissionGroups->sort = 3;
@@ -256,7 +256,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$sortPermissionGroups = new $permission_model;
 		$sortPermissionGroups->name = 'sort-permission-groups';
-		$sortPermissionGroups->display_name = 'Sort Permission Groups';
+		$sortPermissionGroups->display_name = '排序权限组';
 		$sortPermissionGroups->system = true;
 		$sortPermissionGroups->group_id = 4;
 		$sortPermissionGroups->sort = 4;
@@ -270,7 +270,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$createPermissions = new $permission_model;
 		$createPermissions->name = 'create-permissions';
-		$createPermissions->display_name = 'Create Permissions';
+		$createPermissions->display_name = '创建权限';
 		$createPermissions->system = true;
 		$createPermissions->group_id = 4;
 		$createPermissions->sort = 5;
@@ -281,7 +281,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$editPermissions = new $permission_model;
 		$editPermissions->name = 'edit-permissions';
-		$editPermissions->display_name = 'Edit Permissions';
+		$editPermissions->display_name = '编辑权限';
 		$editPermissions->system = true;
 		$editPermissions->group_id = 4;
 		$editPermissions->sort = 6;
@@ -292,7 +292,7 @@ class PermissionTableSeeder extends Seeder {
 		$permission_model = config('access.permission');
 		$deletePermissions = new $permission_model;
 		$deletePermissions->name = 'delete-permissions';
-		$deletePermissions->display_name = 'Delete Permissions';
+		$deletePermissions->display_name = '删除权限';
 		$deletePermissions->system = true;
 		$deletePermissions->group_id = 4;
 		$deletePermissions->sort = 7;
