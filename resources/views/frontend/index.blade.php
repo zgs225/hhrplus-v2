e06b02b99343e1fdc3baf4012fdacd354b4f150a
zgs225 <zgs225@gmail.com>
2015-11-22 14:22:42 +0800

    完善手机版显示

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -34,23 +34,23 @@ $('.hhrplus-clients, .hhrplus-partners').slick({
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
+        slidesToScroll: 3
       }
     },
     {
       breakpoint: 600,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 2,
+        autoplay      : true,
       }
     },
     {
       breakpoint: 480,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autoplay      : true,
       }
     }
   ]
