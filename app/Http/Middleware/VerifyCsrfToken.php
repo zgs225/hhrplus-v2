c8f358ceee28ca4d4dbb4cb3beb12818a8027c01
zgs225 <zgs225@gmail.com>
2015-12-01 16:08:47 +0800

    修复Alipay支付完异步调用时Csrf的问题

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -7,17 +7,26 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
  * Class VerifyCsrfToken
  * @package App\Http\Middleware
  */
-class VerifyCsrfToken extends BaseVerifier {
+class VerifyCsrfToken extends BaseVerifier
+{
+  /**
+   * The URIs that should be excluded from CSRF verification.
+   *
+   * @var array
+   */
+	protected $except = [
+    'payment/notify'
+  ];
 
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next)
-	{
-		return parent::handle($request, $next);
-	}
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request $request
+   * @param  \Closure $next
+   * @return mixed
+   */
+  public function handle($request, Closure $next)
+  {
+    return parent::handle($request, $next);
+  }
 }
