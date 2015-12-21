5f9bc95f2b7d2c5c91722fc17c124cd95ad82008
zgs225 <zgs225@gmail.com>
2015-12-21 11:02:09 +0800

    添加谷歌统计

--- resources/views/includes/partials/ga.blade.php
+++ resources/views/includes/partials/ga.blade.php
@@ -4,7 +4,12 @@
   (function() {
     var hm = document.createElement("script");
     hm.src = "//hm.baidu.com/hm.js?c8919a5aa254a8c98998e621339a95e5";
-    var s = document.getElementsByTagName("script")[0]; 
+    var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
   })();
+</script>
+
+<!-- Google Analytics -->
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-58537045-4', 'auto'); ga('send', 'pageview'); 
 </script>
